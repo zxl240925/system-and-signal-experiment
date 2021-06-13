6ed8b4bc2f5844e362a8f5f829bfee081b58d99c
zxl240925 <72991274+zxl240925@users.noreply.github.com>
2021-06-13 13:22:26 +0800

    离散信号的卷积仿真实验

---
+++ test6_1.m
@@ -0,0 +1,8 @@
+k_start = 0;
+k_end = 19;
+k_step = 0.01;
+k1=k_start:k_end;
+k2=k1;
+f1=k1;
+f2=ones(size(k2));
+exp1_dconv4(f1,f2,k1,k2);
---
+++ test6_2.m
@@ -0,0 +1,9 @@
+k_start = 0;
+k_end = 19;
+k_step = 0.01;
+k1=k_start:k_end;
+k2=k1;
+f1=k1;
+f2=zeros(size(k2));
+f2(1:3)=1;
+exp1_dconv4(f1,f2,k1,k2);
